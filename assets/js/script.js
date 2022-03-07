87dc00a60e43ee9f734d0170c99e8d13d380c0a3
soulslurpee <charles.d.thompson87@gmail.com>
2022-03-06 20:05:33 -0500

    added more HTML, reworked js to match, added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,42 @@ var startButtonEl = document.querySelector("#start-btn");
 var questionBoxEl = document.querySelector("#question-box")
 var mainEl = document.querySelector("main");
 var questionTextEl = document.querySelector("#question-text")
+var btn1El = document.querySelector("#btn1");
+var btn2El = document.querySelector("#btn2");
+var btn3El = document.querySelector("#btn3");
+var btn4El = document.querySelector("#btn4");
+var checkAnswerEl = document.querySelector("#answer-check");
+var timerEl = document.querySelector("#timer");
+var time = document.querySelector("#timer").textContent;
+var playerScore = 0;
+
 var qID = 0;
+
+var startTimer = function (duration, display) {
+    
+    var timer = duration, seconds;
+
+    setInterval(function(){
+        seconds = parseInt(timer % 60, 10);
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+};
 
 const questions = [
     {
     qID: 0,
     q: "This is the first question we will ask. The correct answer is the 1st Answer",
     a: [{ text: "First Answer", isCorrect: true },
-        { text: "2nd Answer", isCorrect: false },
-        { text: "3rd Answer", isCorrect: false },
-        { text: "4th Answer", isCorrect: false },
+        { text: "2nd Answer", isCorrect: "" },
+        { text: "3rd Answer", isCorrect: "" },
+        { text: "4th Answer", isCorrect: "" },
         ]
     },
 
@@ -20,9 +46,9 @@ const questions = [
     qID: 1,
     q: "This is the second question we will ask. The correct answer is the 1st Answer",
     a: [{ text: "1st Answer", isCorrect: true },
-        { text: "2nd Answer", isCorrect: false },
-        { text: "3rd Answer", isCorrect: false },
-        { text: "4th Answer", isCorrect: false },
+        { text: "2nd Answer", isCorrect: "" },
+        { text: "3rd Answer", isCorrect: "" },
+        { text: "4th Answer", isCorrect: "" },
         ]
     },
     
@@ -31,28 +57,72 @@ const questions = [
         qID: 2,
         q: "This is the third question we will ask. The correct answer is the 1st Answer",
         a: [{ text: "1st Answer", isCorrect: true },
-            { text: "2nd Answer", isCorrect: false },
-            { text: "3rd Answer", isCorrect: false },
-            { text: "4th Answer", isCorrect: false },
+            { text: "2nd Answer", isCorrect: "" },
+            { text: "3rd Answer", isCorrect: "" },
+            { text: "4th Answer", isCorrect: "" },
             ]
     },
 
-]
+];
+
+var beginTimer = function () {
+    display = document.querySelector('#timer');
+startTimer(60, display);
+}
 
 var startButtonHandler = function() {
 
     startMenuEl.remove();
-    createQuestion();
+    createQuestion(qID);
 };
 
-var createQuestion = function (qID) {
-    var questionText = questions[0][1];
-    questionTextEl.textContent = ""+questionText+"";
+var createQuestion = function () {
+    var questionText = JSON.stringify(questions[qID].q);
+    questionTextEl.textContent = questionText;
 
+    btn1El.textContent = JSON.stringify(questions[qID].a[0].text);
+    btn2El.textContent = JSON.stringify(questions[qID].a[1].text);
+    btn3El.textContent = JSON.stringify(questions[qID].a[2].text);
+    btn4El.textContent = JSON.stringify(questions[qID].a[3].text);
 
-}
+    btn1El.setAttribute("correct", JSON.stringify(questions[qID].a[0].isCorrect))
+    btn2El.setAttribute("correct", JSON.stringify(questions[qID].a[1].isCorrect))
+    btn3El.setAttribute("correct", JSON.stringify(questions[qID].a[2].isCorrect))
+    btn4El.setAttribute("correct", JSON.stringify(questions[qID].a[3].isCorrect))
 
+    
+};
 
-console.log(qID);
+console.log(qID)
 
+var checkAnswer = function(event) {
+
+    if (event.target.getAttribute("correct") == "true")  {
+        console.log(checkAnswerEl.textContent);
+        checkAnswerEl.textContent = "Correct!";
+    } else {
+        checkAnswerEl.textContent = "Wrong!";
+    };
+
+    qID = qID+1;
+
+    var qIDstop = questions.length-1;
+    console.log(qIDstop);
+
+    if (qID > qIDstop) {
+        //take to high score page
+        console.log("YOU DONE");
+        return;
+    } else {
+        console.log(qID)
+        createQuestion(qID);
+    }
+
+};
+
+startButtonEl.addEventListener("click", beginTimer);
 startButtonEl.addEventListener("click", startButtonHandler);
+btn1El.addEventListener("click", checkAnswer);
+btn2El.addEventListener("click", checkAnswer);
+btn3El.addEventListener("click", checkAnswer);
+btn4El.addEventListener("click", checkAnswer);
